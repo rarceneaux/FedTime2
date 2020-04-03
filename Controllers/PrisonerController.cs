91a154f1040e0f7dea89a74ed88a363940e59508
Raymond <raymond.techcoder@gmail.com>
2020-04-02 22:31:23 -0500

    Add project files.

---
+++ Controllers/PrisonerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Fed
+
+namespace FedTime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrisonerController : ControllerBase
+    {
+    }
+}
---
+++ Models/Prisoner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FedTime.Models
+{
+    public class Prisoner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Charge { get; set; }
+        public string Location { get; set; }
+    }
+}
